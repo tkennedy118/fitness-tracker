d5d0d7da4730e7f56c3891654c191d993db55cdb
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-09 08:56:35 -0500

    add server file information

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-// dependencies
+// Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
+const logger = require('morgan');
 
+// App usage
+const app = express();
 const PORT = process.env.PORT || 8080;
-const db = require('./seeders');
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Get database models and connect to database
+const db = require('./models');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Routes
+require('./controller/routes.js')(app);
+
+// Start express application
+app.listen(PORT, () => {
+  console.log(`App running on PORT ${PORT}`);
+});
