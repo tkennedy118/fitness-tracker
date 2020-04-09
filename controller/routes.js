5d632df088deb6f062d24c38fd641711c822569c
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-09 09:47:45 -0500

    add routes to get one exercise, update one exercise, and create one exercise

--- controller/routes.js
+++ controller/routes.js
@@ -4,11 +4,41 @@ const db = require('../models');
 module.exports = function(app) {
 
   // GET  /api/workouts
-  db.Workout.
+  app.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).limit(1).sort({ day: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(error => {
+        res.json(error);
+      });
+  });
 
   // PUT  /api/workouts
+  app.put('/api/workouts', ({ body }, res) => {
+    db.Exercise.create(body)
+      .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(error => {
+        res.json(error);
+      });
+  });
 
   // POST /api/workouts
+  app.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(error => {
+        res.json(error);
+      });
+  });
 
   // GET  /api/workouts/range
+  app.get('api/workouts/range', (req, res) => {
+
+  });
 };
